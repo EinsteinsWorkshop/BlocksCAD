3ccb745d3bbaac9ca3910056c73e212b0e659653
Matthew Minuti <matt.minuti@gmail.com>
2015-09-18 17:45:40 -0400

    updating standalone. not ready for use.

--- build-standalone.js
+++ build-standalone.js
@@ -1,13 +1,16 @@
 var NwBuilder = require('nw-builder');
 var nw = new NwBuilder({
-    files: ['**',  '!cache/**', '!build/**'], // get everything except the nwbuild stuff
+//    files: ['**',  '!cache/**', '!build/**'], // get everything except the nwbuild stuff
 //    files: ['**'], // get everything
 //    files: ['**', '!closure-library/**', '!_soy/**', '!node_modules/**', '!openscad-openjscad-translator/**', '!cache/**', '!build/**'], // use the glob format
-//    files: ['blockly/*compressed.js', 'blockscad/*compressed.js', 'blockscad/style.css', 'bootstrap/**', 'docs/**', 'fonts/**', 'imgs/**', 'jquery/**', 'opentype/**', 'credits.html', 'favicon.ico', 'icons.png', 'index.html', 'package.json', 'privacy.html', 'TOS.html' ], // real way to do it
+    files: ['blockly/*compressed.js', 'blockscad/*compressed.js', 'blockscad/style.css', 'bootstrap/**', 'docs/**', 'fonts/**', 'imgs/**', 'jquery/**', 'opentype/**', 'favicon.ico', 'icons.png', 'index.html', 'package.json', 'privacy.html', 'TOS.html', 'icon128.ico', 'icon128.png', 'fonts/**', 'blockly/media/**', 'blockscad/csg.js', 'blockscad/formats.js', 'blockscad/viewer.js' ], // real way to do it
     platforms: ['osx', 'win', 'linux'],
 //    version: '0.12.1'
     version: '0.12.3',
-    winIco: 'icon128.ico'
+    winIco: 'icon128.ico',
+    window: {
+      icon: 'icon128.png'
+   }
 });
 
 //Log stuff you want
