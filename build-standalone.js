bf893793ca20bbc2ff95db9d030e83f1c85b8036
Jennie Yoder <jennie_yoder@yahoo.com>
2016-03-03 10:49:29 -0500

    fixed linear extrude example added 32 bit windows version to build types

--- build-standalone.js
+++ build-standalone.js
@@ -4,7 +4,7 @@ var nw = new NwBuilder({
 //    files: ['**'], // get everything
    // files: ['**', '!closure-library/**', '!_soy/**', '!node_modules/**', '!openscad-openjscad-translator/**', '!cache/**', '!build/**'], // use the glob format
     files: ['fonts/**', 'lie/lie.polyfill.min.js', 'bootbox/bootbox.min.js', 'auth.js', 'blockly/blockly_compressed.js','blockly/blocks_compressed.js','jquery/jquery-1.11.3.min.js', 'jquery/jquery-ui.min.js','jquery/jquery.hammer.js', 'jquery/jquery.ui.touch-punch.min.js', 'bootstrap/bootstrap-3.3.4-dist/css/bootstrap.min.css', 'bootstrap/bootstrap-3.3.4-dist/css/bootstrap-theme.min.css', 'bootstrap/bootstrap-3.3.4-dist/js/bootstrap.min.js', 'blockly/openscad_compressed.js', 'blockscad/viewer_compressed.js', 'blockscad/blockscad_compressed.js', 'blockscad/underscore.js', 'blockscad/openscad-openjscad-translator.js', 'blockscad/style.css', 'favicon.ico', 'ewicon.png', 'imgs/**', 'docs/**', 'icon128.ico', 'icon128.png', 'blockly/media/**', 'index.html', 'package.json', 'blockly/msg/messages.js', 'blockly/msg/js/en.js', 'blockly/msg/json/en.json', 'TOS.html', 'privacy.html', 'gpl-3.0-standalone.html', 'examples/**', 'opentype/dist/opentype.js'],
-    platforms: ['win64'],
+    platforms: ['win64', 'win32'],
     // platforms: ['win64', 'linux'],
    // version: '0.12.1',
     version: '0.12.3',
