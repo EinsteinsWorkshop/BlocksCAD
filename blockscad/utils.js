2578dcb7a8d17771b0452aa51168fb6a77dbcd41
Jennie Yoder <jennie_yoder@yahoo.com>
2017-03-07 10:42:34 -0800

    added zh-hans translation

--- blockscad/utils.js
+++ blockscad/utils.js
@@ -10,7 +10,8 @@ var BSUtils = BSUtils || {};
 BSUtils.LANGUAGE_NAME = {
   'en': 'English',
   'de': 'Deutsch',
-  'fr': 'Français'
+  'fr': 'Français',
+  'zh-hans': '簡體中文',
   // 'es': 'Español'
 };
 // BSUtils.LANGUAGE_NAME = {
